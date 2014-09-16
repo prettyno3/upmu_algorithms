11dd0d505f9270fd1c6af329d134e8c857c81593
Connor Brooks <cbrooks@berkeley.edu>
2014-09-16 14:54:49 -0700

    Update example_delta.py

--- MPA/example_delta.py
+++ MPA/example_delta.py
@@ -24,7 +24,7 @@ class ExampleDelta(qdf.QuasarDistillate):
 
         #If this is incremented, it is assumed that the whole distillate is invalidated, and it
         #will be deleted and discarded. In addition all 'persist' data will be removed
-        self.set_version(3)
+        self.set_version(4)
 
     @defer.inlineCallbacks
     def compute(self):
