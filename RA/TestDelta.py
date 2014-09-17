93515ea6a1bb4ef06d74c3a004d760fda6b6ca88
Reza Arghandeh <arghandeh@users.noreply.github.com>
2014-09-17 16:31:53 -0700

    Its a test code for delta function

--- RA/TestDelta.py
+++ RA/TestDelta.py
@@ -14,7 +14,7 @@ class DistillateDriver(qdf.QuasarDistillate):
         self.set_author("MPA")
 
         #This is the second level. This name should be unique for every algorithm you write
-        self.set_name("Delta4")
+        self.set_name("Delta5")
 
         #This is the final level. You can have multiple of these
         self.add_stream("delta", unit="Degrees")
