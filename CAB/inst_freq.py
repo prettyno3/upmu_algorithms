8fd4c20ff7de45d5d7121109436314e457d9a7e6
Connor Brooks <cbrooks@berkeley.edu>
2014-09-25 14:49:55 -0700

    Implemented 1 second interval

--- CAB/inst_freq.py
+++ CAB/inst_freq.py
@@ -49,19 +49,28 @@ class Inst_Freq(qdf.QuasarDistillate):
 
         input_version, input_phases = yield self.stream_get(self.input_stream, start_date, end_date)
         inst_freqs = []
+        sampling_freq = 160 #Hz
 
         i = 0
-        while i < len(input_phases)-1:
-            phase_diff = input_phases[i+1].value - input_phases[i].value
-            delta = input_phases[i+1].time - input_phases[i].time
-            if delta == 0:
+        while i < len(input_phases)-sampling_freq:
+            delta_samples = sampling_freq #upper bound
+            t1 = input_phases[i].time
+            t2 = input_phases[i+delta_samples].time
+            while ((t2 - t1) > 1e9 && t2 > t1: #catch zeroed or missing samples
+                delta_samples -= 1 #decrement ~one sample per missing sample in interval
+                t2 = input_phases[i+delta_samples].time
+            x1 = input_phases[i].value
+            x2 = input_phases[i+delta_samples]
+            phase_diff = x2 - x1
+            delta_time = t2 - t1
+            if delta_time == 0:
                 i += 1
                 continue
             if phase_diff > 180:
                 phase_diff -= 360
             elif phase_diff < -180:
                 phase_diff += 360
-            inst_freqs.append((input_phases[i].time, (phase_diff/delta)*1e9))
+            inst_freqs.append(t1, (phase_diff/delta_time)*1e9))
             if len(inst_freqs) >= qdf.OPTIMAL_BATCH_SIZE:
                 yield self.stream_insert_multiple(self.output_stream, inst_freqs)
                 inst_freqs = []
