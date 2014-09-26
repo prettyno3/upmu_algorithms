a4ad61f3cbe7bfade1836a3452267f7e6f1a8fdb
Connor Brooks <cbrooks@berkeley.edu>
2014-09-26 13:48:09 -0700

    Set values for soda_a L1MAG

--- CAB/scale_amp.py
+++ CAB/scale_amp.py
@@ -11,8 +11,8 @@ class ExampleScale(qdf.QuasarDistillate):
         This constructs your distillate algorithm
         """
         #TEMP. In future, find dynamically
-        self.input_name = "1hz"
-        input_uid = "2942433b-6511-4298-a0b2-2886456cff4f"
+        self.input_name = "L1MAG"
+        input_uid = "abffcf07-9e17-404a-98c3-ea4d60042ff3"
         version = 1
 
         #This is the first level in the distillate tree
@@ -22,7 +22,7 @@ class ExampleScale(qdf.QuasarDistillate):
         self.set_name("Scale_Amp")
 
         #This is the final level. You can have multiple of these
-        self.add_stream("Scale_Amp", unit="Amp_Unit")
+        self.add_stream("soda_L1Mag_4.5", unit="V")
 
         self.use_stream(self.input_name, input_uid)
 
@@ -43,9 +43,9 @@ class ExampleScale(qdf.QuasarDistillate):
             return
 
         #TEMP. In future, find dynamically
-        start_date = self.date("2014-08-17T00:00:00.000000")
-        end_date = self.date("2014-08-17T00:15:00.000000")
-        scale_factor = 2.5
+        start_date = self.date("2014-09-07T00:00:00.000000")
+        end_date = self.date("2014-09-07T06:00:00.000000")
+        scale_factor = 4.5
 
         input_version, input_values = yield self.stream_get(self.input_name, start_date, end_date)
         scaled_values = []
