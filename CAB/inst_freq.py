ae79db07a16da3a5f758518c6f44b4559cf5ce34
Michael Andersen <michael@steelcode.com>
2014-09-27 17:23:40 -0700

    Update inst_freq.py

--- CAB/inst_freq.py
+++ CAB/inst_freq.py
@@ -44,8 +44,8 @@ class Inst_Freq(qdf.QuasarDistillate):
             return
 
         #TEMP. In future, find dynamically
-        start_date = self.date("2014-09-07T00:00:00.000000")
-        end_date = self.date("2014-09-07T04:00:00.000000")
+        start_date = self.date("2014-09-23T00:00:00.000000")
+        end_date = self.date("2014-09-23T04:00:00.000000")
 
         input_version, input_phases = yield self.stream_get(self.input_stream, start_date, end_date)
         inst_freqs = []
